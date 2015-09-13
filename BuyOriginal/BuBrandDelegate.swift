dc4fc01bd3c00a6415c049e26e43ac7786233f62
mehr.najafi@gmail.com <mehr.najafi@gmail.com>
2015-09-13 11:48:49 -0400

    no message

---
+++ BuyOriginal/BuBrandDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  BuBrandDelegate.swift
+//  BuyOriginal
+//
+//  Created by mehran najafi on 2015-09-02.
+//  Copyright (c) 2015 MandM. All rights reserved.
+//
+
+import Foundation
---
+++ SimulatorUtility.swift
@@ -0,0 +1,9 @@
+//
+//  SimulatorUtilities.swift
+//  BuyOriginal
+//
+//  Created by mehran najafi on 2015-09-10.
+//  Copyright (c) 2015 MandM. All rights reserved.
+//
+
+import Foundation
